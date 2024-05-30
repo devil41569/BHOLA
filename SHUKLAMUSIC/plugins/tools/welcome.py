6c498714e5f0599f24883467b29c76a8c3572b0e
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-30 11:31:22 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -1,87 +1,18 @@
-from SHUKLAMUSIC import app
-from pyrogram.errors import RPCError
-from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
-from os import environ
-from typing import Union, Optional
-from PIL import Image, ImageDraw, ImageFont
-from os import environ
-import random
-from pyrogram import Client, filters
-from pyrogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboardMarkup
-from PIL import Image, ImageDraw, ImageFont
-import asyncio, os, time, aiohttp
-from pathlib import Path
-from PIL import Image, ImageDraw, ImageFont, ImageEnhance
-from asyncio import sleep
-from pyrogram import filters, Client, enums
-from pyrogram.enums import ParseMode
-from logging import getLogger
-from SHUKLAMUSIC.utils.Shukla_ban import admin_filter
+import os
+from unidecode import unidecode
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from pyrogram import Client, filters
-import requests
-import random
-import os
-import re
-import asyncio
-import time
-from SHUKLAMUSIC.utils.database import add_served_chat
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from SHUKLAMUSIC.utils.database import get_assistant
-import asyncio
+from SHUKLAMUSIC import LOGGER
+from pyrogram.types import Message
 from SHUKLAMUSIC.misc import SUDOERS
-from SHUKLAMUSIC.mongo.afkdb import PROCESS
-from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
 from SHUKLAMUSIC import app
-import asyncio
-import random
-from pyrogram import Client, filters
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import (
-    ChatAdminRequired,
-    InviteRequestSent,
-    UserAlreadyParticipant,
-    UserNotParticipant,
-)
-from SHUKLAMUSIC.utils.database import get_assistant, is_active_chat
-
-
-
-random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
-]
-# --------------------------------------------------------------------------------- #
-
-
-
-
+from SHUKLAMUSIC.CuteDb.Weldb import *
+from config import LOGGER_ID
 
 LOGGER = getLogger(__name__)
 
-class WelDatabase:
-    def __init__(self):
-        self.data = {}
-
-    async def find_one(self, chat_id):
-        return chat_id in self.data
-
-    async def add_wlcm(self, chat_id):
-        if chat_id not in self.data:
-            self.data[chat_id] = {"state": "on"}  # Default state is "on"
-
-    async def rm_wlcm(self, chat_id):
-        if chat_id in self.data:
-            del self.data[chat_id]
-
-wlcm = WelDatabase()
 
 class temp:
     ME = None
@@ -91,11 +22,8 @@ class temp:
     U_NAME = None
     B_NAME = None
 
-
-
-def circle(pfp, size=(500, 500), brightness_factor=10):
+def circle(pfp, size=(450, 450)):
     pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
-    pfp = ImageEnhance.Brightness(pfp).enhance(brightness_factor)
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
     mask = Image.new("L", bigsize, 0)
     draw = ImageDraw.Draw(mask)
@@ -105,31 +33,31 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     pfp.putalpha(mask)
     return pfp
 
-def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
+def welcomepic(pic, user, chat, id, uname):
     background = Image.open("SHUKLAMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
-    pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((635, 635))
+    pfp = circle(pfp)
+    pfp = pfp.resize(
+        (450, 450)
+    ) 
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=70)
-    welcome_font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=61)
-    #draw.text((630, 540), f'ID: {id}', fill=(255, 255, 255), font=font)
-    #
- #   draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
-    draw.text((2999, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
-#    draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
-  #  draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
-
-    #
-    pfp_position = (332, 323)
-    background.paste(pfp, pfp_position, pfp)
-    background.save(f"downloads/welcome#{id}.png")
+    font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=49)
+    font2 = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=90)
+    draw.text((65, 250), f'NAME : {unidecode(user)}', fill="black", font=font)
+    draw.text((65, 340), f'ID : {id}', fill="black", font=font)
+    draw.text((65, 430), f"USERNAME : {uname}", fill="black",font=font)
+    pfp_position = (767, 133)  
+    background.paste(pfp, pfp_position, pfp)  
+    background.save(
+        f"downloads/welcome#{id}.png"
+    )
     return f"downloads/welcome#{id}.png"
 
 
-@app.on_message(filters.command("welcome") & ~filters.private)
+HUHU = """**
+@app.on_message(filters.command("swel") & ~filters.private)
 async def auto_state(_, message):
-    usage = "**ᴜsᴀɢᴇ:**\n**⦿ /welcome [on|off]**"
+    usage = "**❖ ᴜsᴀɢᴇ ➥** /swel [ᴇɴᴀʙʟᴇ|ᴅɪsᴀʙʟᴇ]"
     if len(message.command) == 1:
         return await message.reply_text(usage)
     chat_id = message.chat.id
@@ -138,121 +66,92 @@ async def auto_state(_, message):
         enums.ChatMemberStatus.ADMINISTRATOR,
         enums.ChatMemberStatus.OWNER,
     ):
-        A = await wlcm.find_one(chat_id)
-        state = message.text.split(None, 1)[1].strip().lower()
-        if state == "off":
-            if A:
-                await message.reply_text("**ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ !**")
-            else:
-                await wlcm.add_wlcm(chat_id)
-                await message.reply_text(f"**ᴅɪsᴀʙʟᴇᴅ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ ɪɴ** {message.chat.title}")
-        elif state == "on":
-            if not A:
-                await message.reply_text("**ᴇɴᴀʙʟᴇ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ.**")
-            else:
-                await wlcm.rm_wlcm(chat_id)
-                await message.reply_text(f"**ᴇɴᴀʙʟᴇᴅ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ ɪɴ ** {message.chat.title}")
-        else:
-            await message.reply_text(usage)
+      A = await wlcm.find_one({"chat_id" : chat_id})
+      state = message.text.split(None, 1)[1].strip()
+      state = state.lower()
+      if state == "enable":
+        if A:
+           return await message.reply_text("✦ Special Welcome Already Enabled")
+        elif not A:
+           await add_wlcm(chat_id)
+           await message.reply_text(f"✦ Enabled Special Welcome in {message.chat.title}")
+      elif state == "disable":
+        if not A:
+           return await message.reply_text("✦ Special Welcome Already Disabled")
+        elif A:
+           await rm_wlcm(chat_id)
+           await message.reply_text(f"✦ Disabled Special Welcome in {message.chat.title}")
+      else:
+        await message.reply_text(usage)
     else:
-        await message.reply("**sᴏʀʀʏ ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴇɴᴀʙʟᴇ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ!**")
-
-
+        await message.reply("✦ Only Admins Can Use This Command")
+  **  """
+#bhag 
 
 @app.on_chat_member_updated(filters.group, group=-3)
-async def greet_new_member(_, member: ChatMemberUpdated):
+async def greet_group(_, member: ChatMemberUpdated):
     chat_id = member.chat.id
-    count = await app.get_chat_members_count(chat_id)
-    A = await wlcm.find_one(chat_id)
-    if A:
+   # A = await wlcm.find_one({"chat_id" : chat_id})
+   # if not A:
+  #     return
+    if (
+        not member.new_chat_member
+        or member.new_chat_member.status in {"banned", "left", "restricted"}
+        or member.old_chat_member
+    ):
         return
-
     user = member.new_chat_member.user if member.new_chat_member else member.from_user
-    
-    # Add the modified condition here
-    if member.new_chat_member and not member.old_chat_member and member.new_chat_member.status != "kicked":
-    
+    try:
+        pic = await app.download_media(
+            user.photo.big_file_id, file_name=f"pp{user.id}.png"
+        )
+    except AttributeError:
+        pic = "SHUKLAMUSIC/assets/upic.png"
+    if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
         try:
-            pic = await app.download_media(
-                user.photo.big_file_id, file_name=f"pp{user.id}.png"
-            )
-        except AttributeError:
-            pic = "SHUKLAMUSIC/assets/upic.png"
-        if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
-            try:
-                await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
-            except Exception as e:
-                LOGGER.error(e)
-        try:
-            welcomeimg = welcomepic(
-                pic, user.first_name, member.chat.title, user.id, user.username
-            )
-            button_text = "๏ ᴠɪᴇᴡ ɴᴇᴡ ᴍᴇᴍʙᴇʀ ๏"
-            add_button_text = "❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ"
-            deep_link = f"tg://openmessage?user_id={user.id}"
-            add_link = f"https://t.me/ITZ_IND_CODER"
-            temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
-                member.chat.id,
-                photo=welcomeimg,
-                caption=f"""
-**❅────✦ ᴡᴇʟᴄᴏᴍᴇ ✦────❅**
-
-▰▱▱▱▱▱▱▱▱▱▱▱
-▱▱▱▱▰
-
-**➻ ɴᴀᴍᴇ »** {user.mention}
-**➻ ɪᴅ »** `{user.id}`
-**➻ ᴜ_ɴᴀᴍᴇ »** @{user.username}
-**➻ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs »** {count}
-
-▱▱▱▱▰
-▰▱▱▱▱▱▱▱▱▱▱▱
-
-**❅─────✧❅✦❅✧─────❅**
-""",
-                reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton(button_text, url=deep_link)],
-                    [InlineKeyboardButton(text=add_button_text, url=add_link)],
-                ])
-            )
+            await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
         except Exception as e:
             LOGGER.error(e)
-
-
-@app.on_message(filters.command("gadd") & filters.user(6664582540))
-async def add_all(client, message):
-    command_parts = message.text.split(" ")
-    if len(command_parts) != 2:
-        await message.reply("**⚠️ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd bot username`**")
-        return
-    
-    bot_username = command_parts[1]
     try:
-        userbot = await get_assistant(message.chat.id)
-        bot = await app.get_users(bot_username)
-        app_id = bot.id
-        done = 0
-        failed = 0
-        lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
-        
-        async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002114104208:
-                continue
-            try:
-                await userbot.add_chat_members(dialog.chat.id, app_id)
-                done += 1
-                await lol.edit(
-                    f"**🔂 ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
-                )
-            except Exception as e:
-                failed += 1
-                await lol.edit(
-                    f"**🔂 ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅɪɴɢ ʙʏ»** @{userbot.username}"
-                )
-            await asyncio.sleep(3)  # Adjust sleep time based on rate limits
-        
-        await lol.edit(
-            f"**➻ {bot_username} ʙᴏᴛ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ🎉**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
+        welcomeimg = welcomepic(
+            pic, user.first_name, member.chat.title, user.id, user.username
         )
+        temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
+            member.chat.id,
+            photo=welcomeimg,
+            caption= f"""
+  
+❁══❰🌺𝐖𝐄𝐋𝐂𝐎𝐌𝐄🌺❱═❍⊱❁
+      ᴍᴇᴍʙᴇʀ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ
+╔══════ஜ۩۞۩ஜ════╗
+● ɴᴀᴍᴇ ➥  {user.mention}
+         
+● ᴜsᴇʀ ɪᴅ ➥  {user.id}
+╚══════ஜ۩۞۩ஜ════╝ 
+
+             Sᴜᴘᴘᴏʀᴛ
+╔══════ஜ۩۞۩ஜ════╗            
+════════════════❍⊱❁            
+╭✠╼━━━━━━❖━━━━━━━✠╮ 
+│Sᴜᴘᴘᴏʀᴛ➥ [Sᴜᴘᴘᴏʀᴛ](https://t.me/the_ind_coders)
+╰✠╼━━━━━━❖━━━━━━━✠╯
+════════════════❍⊱❁
+╚══════ஜ۩۞۩ஜ════╝
+""",
+reply_markup=InlineKeyboardMarkup(
+[
+[InlineKeyboardButton(f"❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ", url=f"https://t.me/ITZ_IND_CODER"),
+]
+]
+))
+
     except Exception as e:
-        await message.reply(f"Error: {str(e)}")
+        LOGGER.error(e)
+    try:
+        os.remove(f"downloads/welcome#{user.id}.png")
+        os.remove(f"downloads/pp{user.id}.png")
+    except Exception as e:
+        pass
+
+
+                                             
