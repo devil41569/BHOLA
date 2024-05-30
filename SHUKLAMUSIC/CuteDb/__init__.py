0a0a67797c29a34eca220a3ff896198f70c39714
ishu9805 <abhishek47903@gmail.com>
2024-05-30 12:30:16 +0530

    Update __init__.py

--- SHUKLAMUSIC/CuteDb/__init__.py
+++ SHUKLAMUSIC/CuteDb/__init__.py
@@ -1,7 +1,8 @@
-from motor.motor_asyncio import AsyncClient
+from motor.motor_asyncio import AsyncIOMotorClient
+
 from config import MONGO_DB_URI
 
 DBNAME = "CUTIEXMUSICBOTPT"
 
-mongo = AsyncClient(MONGO_DB_URI)
+mongo = AsyncIOMotorClient(MONGO_DB_URI)
 dbname = mongo[DBNAME]
